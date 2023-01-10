224309a82da511ac69210efb11a3eff3c32e82a8
alissens <39267930+alissens@users.noreply.github.com>
2023-01-10 15:39:28 +0000

    icon added

--- DailyGoals/DailyGoals/ContentView.swift
+++ DailyGoals/DailyGoals/ContentView.swift
@@ -65,9 +65,7 @@ struct ContentView: View {
 				Label("Copy to clipboard", systemImage: "doc.on.clipboard")
 			}
 			.padding()
-			.alert("You're good", isPresented: $showCopySuccess) {
-				Button("OK") {}
-			}
+            .alert("You're good", isPresented: $showCopySuccess) {}
 		}
 	}
 
