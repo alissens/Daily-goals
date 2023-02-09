bf7b47dafd90fd489fc5e7723af976ed66ec935b
Marcello Mascia <marcellomasciamac.com>
2023-02-09 22:39:52 +0000

    Closing the app when the window is closed

--- DailyGoals/DailyGoals/DailyGoalsApp.swift
+++ DailyGoals/DailyGoals/DailyGoalsApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+
+		true
+	}
+}
 
 @main
 struct DailyGoalsApp: App {
+
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
