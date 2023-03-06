26e6fe11519955ef1155d9507e4102a7f4ab3a96
Alisa <39267930+alissens@users.noreply.github.com>
2023-03-06 11:46:59 +0000

    Update DailyGoals/DailyGoals/Utilities/DailyGoalsGenerator.swift

--- DailyGoals/DailyGoals/Utilities/DailyGoalsGenerator.swift
+++ DailyGoals/DailyGoals/Utilities/DailyGoalsGenerator.swift
@@ -24,7 +24,7 @@ struct DailyGoalsGenerator {
 
 		text += "\n"
 
-		for goal in goals where goal.action != .additional {
+        for goal in goals where goal.action != .additional {
 			text += goal.text
         }
 
